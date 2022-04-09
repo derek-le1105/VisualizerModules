fa6e4e9a3f188b707f67421eee53aa83601abef2
derek-le1105 <derekle1105@gmail.com>
2022-04-09 00:20:35 -0700

    Pushed everything to repo

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const sortingBtn = document.getElementById('sorting-card');
+
+if(sortingBtn){
+    sortingBtn.addEventListener('click', function(){
+        console.log("hello!");
+    });
+}
