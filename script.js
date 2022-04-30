56b5ea2fcb229cc1a471c84d0fe02d36be07a843
Derek Le <derekle1105@gmail.com>
2022-04-30 16:01:19 -0700

    Implented hidden class and active navbar

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 'use strict';
 
-const sortingBtn = document.getElementById('sorting-card');
+const moduleCards = document.getElementsByClassName("card"); 
+const navbarLinks = document.getElementsByClassName("nav")
+const title = document.getElementById("title-name");
+console.log('title: ', navbarLinks[0].id);
 
-if(sortingBtn){
-    sortingBtn.addEventListener('click', function(){
-        console.log("hello!");
-    });
+function sortingVisualizer(){
+    for(let i = 0; i < moduleCards.length; ++i)
+        moduleCards[i].classList.add('hidden');
+    for(let i = 0; i < navbarLinks.length; ++i){
+        if(navbarLinks[i].classList.contains('active'))
+            navbarLinks[i].classList.remove('active');
+        if(navbarLinks[i].id == 'sort-nav')
+            navbarLinks[i].classList.add('active')
+    }
+
+    title.classList.add('hidden');
+}
+
+function tilePuzzle(){
+    for(let i = 0; i < moduleCards.length; ++i)
+        moduleCards[i].classList.add('hidden');
+    for(let i = 0; i < navbarLinks.length; ++i){
+        if(navbarLinks[i].classList.contains('active'))
+            navbarLinks[i].classList.remove('active');
+        if(navbarLinks[i].id == 'tile-nav')
+            navbarLinks[i].classList.add('active')
+    }
+
+    title.classList.add('hidden');  
 }
