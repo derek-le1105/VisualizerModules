d3f09af85f6183712159188bc9e454d03ae05960
Derek Le <derekle1105@gmail.com>
2022-04-30 17:50:58 -0700

    Changed module accessing method

--- script.js
+++ script.js
@@ -3,30 +3,3 @@
 const moduleCards = document.getElementsByClassName("card"); 
 const navbarLinks = document.getElementsByClassName("nav")
 const title = document.getElementById("title-name");
-console.log('title: ', navbarLinks[0].id);
-
-function sortingVisualizer(){
-    for(let i = 0; i < moduleCards.length; ++i)
-        moduleCards[i].classList.add('hidden');
-    for(let i = 0; i < navbarLinks.length; ++i){
-        if(navbarLinks[i].classList.contains('active'))
-            navbarLinks[i].classList.remove('active');
-        if(navbarLinks[i].id == 'sort-nav')
-            navbarLinks[i].classList.add('active')
-    }
-
-    title.classList.add('hidden');
-}
-
-function tilePuzzle(){
-    for(let i = 0; i < moduleCards.length; ++i)
-        moduleCards[i].classList.add('hidden');
-    for(let i = 0; i < navbarLinks.length; ++i){
-        if(navbarLinks[i].classList.contains('active'))
-            navbarLinks[i].classList.remove('active');
-        if(navbarLinks[i].id == 'tile-nav')
-            navbarLinks[i].classList.add('active')
-    }
-
-    title.classList.add('hidden');  
-}
